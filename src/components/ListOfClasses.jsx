059af13d6c07c092a97d03b1d56ad1b8a122d006
Emmanuel Atama <emmanuelatama@outlook.com>
2021-11-19 09:59:34 +0000

    Filter Classes

--- src/components/ListOfClasses.jsx
+++ src/components/ListOfClasses.jsx
@@ -1,65 +1,70 @@
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
 
-export default function ListOfClasses(props) {
+export default function ListOfClasses(classes) {
+
+const [classTypes, setClassTypes] = useState("")
+
+console.log("Inside ListOfClasses App State:", classTypes)
 
   const {classes, setClassToProcess, setClasses } = props;
   const API_URL = process.env.REACT_APP_API_URL;
   const navigate = useNavigate();
 
-  const jump = (event) => {
-    event.preventDefault();
-    navigate("/create-class");
-  };
 
-  // const handleClassToProcess = (event, oneClass) => {
-  //   setClassToProcess(oneClass);
-    // navigate("/classes/view/${oneClass.id}");
-  // };
-
-  const handleDelete = (event, oneClass) => {
-
-    fetch(`${API_URL}/classes/${oneClass.id}`, { method: "DELETE" })
-    .then(() => { 
-
-      const removeDeletedClass = classes.filter((cls) => cls.id !== oneClass.id)
-
-      setClasses([...removeDeletedClass]);
-    })
-  };
+    const handleClassFilter = (event) => {
+    
+  console.log("Inside handleClassFilter: ", event.target.value);
+  setClassTypes(event.target.value)
+      }
 
   return (
     <>
-      <main>
-        <div className="padding two-column-grid-expand__right">
-          <h2> Classes</h2>
-          <div>
-            <Button onClick={jump}>
-              <strong> ➕ </strong>
-            </Button>
-          </div>
-        </div>
+      <h2 className="padding-left"> List Of Classes</h2>
+      <form className="filter-form">
+              <label htmlFor="filter-by-classType">
+                <h3>Activity</h3>
+              </label>
+              <select
+                name="fiter-by-activity"
+                id="filter-by-activity"
+                onChange={handleClassFilter}
+                value={classTypes}
+              >
+                <option selected value="">
+                  ...Select Activity...
+                </option>
+                <option value="cardio">Cardio</option>
+                <option value="biking">Biking</option>
+                <option value="boxing">Boxing</option>
+                <option value="zumba">Zumba</option>
+                <option value="yoga">Yoga</option>
+                <option value="running">Running</option>
+              </select>
+            </form>
+      <main className="">
+      
         <ul className="cards padding classes-list">
-          {classes.map((oneClass, index) => {
-            const {
-              className,
-              classType,
-              classStatus,
-              duration,
-            } = oneClass;
-
-            var {classStartDate } = oneClass;
-            classStartDate = classStartDate.toLocaleString("en-GB");
-            console.log("date test: ", classStartDate)
-
+          {classes.filter((classe) => {
+     if (classTypes === classe.classType || classTypes ==="") {
+     return true;
+     } else {
+       return false;
+     }
+    })
+    .map((oneClass, index) => {
             return (
+              
+            
               <li key={index} className="border-for-li two-row-grid">
                 <div>
-                  <h3> <strong> {className} </strong> </h3>
-                  <p>Activity: {classType}</p>
-                  <p>Status: {classStatus}</p>
-                  <p>Date: {classStartDate}</p>
-                  <p>Duration: {duration} min</p>
+                  <h3>
+                    <strong> {oneClass.className} </strong>
+                  </h3>
+                  <p>Activity: {oneClass.classType}</p>
+                  <p>Status: {oneClass.classStatus}</p>
+                  <p>Data: {oneClass.classStartDate}</p>
+                  <p>Duration: {oneClass.duration} min</p>
                 </div>
                 <div className="one-class-dashboard-buttons">
                   {/* <Button
